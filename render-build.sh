b64c41f14b1a662940b0c29928b671895bf9d6ed
jameskocaba <jameskocaba@gmail.com>
2025-12-23 14:26:07 -0500

    Add files via upload

--- render-build.sh
+++ render-build.sh
@@ -1,22 +1,17 @@
 #!/usr/bin/env bash
-# Exit on error
-set -o errexit
 
-# 1. Install Python dependencies
+# Install Python requirements
 pip install -r requirements.txt
 
-# 2. Install FFmpeg (Static Build)
-# We download a version that doesn't require root access to install
-if [ ! -d "ffmpeg_bin" ]; then
-  echo "Downloading FFmpeg..."
-  mkdir -p ffmpeg_bin
-  cd ffmpeg_bin
-  wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
-  tar xvf ffmpeg-release-amd64-static.tar.xz --strip-components=1
-  cd ..
-fi
+# Create folder for ffmpeg
+mkdir -p ffmpeg_bin
+cd ffmpeg_bin
 
-# 3. Add FFmpeg to the PATH so the app can see it
-export PATH=$PATH:$(pwd)/ffmpeg_bin
+# Download and extract FFmpeg
+wget -q https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+tar xf ffmpeg-release-amd64-static.tar.xz --strip-components=1
 
-echo "Build complete!"
+# Clean up the compressed file to save space
+rm ffmpeg-release-amd64-static.tar.xz
+
+echo "Build successful!"
