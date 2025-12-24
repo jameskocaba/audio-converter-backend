acb7c96850997d69f88d0f23f727d6351507981d
jameskocaba <jameskocaba@gmail.com>
2025-12-24 08:34:26 -0500

    Add files via upload

---
+++ render-build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Install Python requirements
+pip install -r requirements.txt
+
+# Create folder for ffmpeg
+mkdir -p ffmpeg_bin
+cd ffmpeg_bin
+
+# Download and extract FFmpeg
+wget -q https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+tar xf ffmpeg-release-amd64-static.tar.xz --strip-components=1
+
+# Clean up the compressed file to save space
+rm ffmpeg-release-amd64-static.tar.xz
+
+echo "Build successful!"
